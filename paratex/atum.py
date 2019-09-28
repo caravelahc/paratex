d1bd3e6feef46f5a0248c1509a57958894d7d0dd
gabriel <gabrielfranksimonetto@gmail.com>
2019-09-28 14:13:25 -0300

    instructions and poetry selenium

--- paratex/atum.py
+++ paratex/atum.py
@@ -2,14 +2,19 @@ from selenium import webdriver
 
 # rodem essa bodega com 
 # "python -i atum.py" ou
-# "ipython -i atum.py"
 # dai da pra ir rodando novos comandos no terminal
 # e ir vendo como o selenium responde, eh bem top
+
+# instalando geckodriver
+# https://askubuntu.com/questions/851401/where-to-find-geckodriver-needed-by-selenium-python-package
 
 class Atum:
     def __init__(self, driver):
         self.driver = driver
         self.url = 'http://transparencia.alesc.sc.gov.br/presenca_plenaria.php'
+
+        # formas de colocar as coisas que a gente quer de verdade
+        # assim que a gente descobrir como elas se parecem
         # self.search_bar = 'lst-ib'  # id
         # self.btn_search = 'btnK'  # name
         # self.btn_lucky = 'btnI'  # name
