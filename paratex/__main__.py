7521696820a97fd98b98a4acc3f7e24241fb715a
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2019-09-28 17:27:30 -0300

    Returning dataframe instead of printing.

--- paratex/__main__.py
+++ paratex/__main__.py
@@ -1,18 +1,15 @@
 import pandas as pd
 import requests
 
-from .extractor import extract_attendance
+from .extractor import extract_attendance, Session
 
 
 def main():
     URL = 'http://transparencia.alesc.sc.gov.br/presenca_plenaria_detalhes.php?id=1783'
     html = load_html(URL)
     session = extract_attendance(html)
-    print(session)
-    visualizing_results(
-        (name, presence, justification, session.date)
-        for name, (presence, justification) in session.attendance.items()
-    )
+    print(session.title)
+    print(as_dataframe(session).head())
 
 
 def load_html(url: str) -> str:
@@ -21,13 +18,17 @@ def load_html(url: str) -> str:
 
 # Intended to be used later on the code
 # will leave this here for utility purposes for now
-def visualizing_results(observations):
+def as_dataframe(session: Session):
+    observations = (
+        (name, presence, justification, session.date)
+        for name, (presence, justification) in session.attendance.items()
+    )
     df = pd.DataFrame(observations)
     df = df.rename(
         {0: 'Nome', 1: 'Presenca', 2: 'Justificativa', 3: 'Data'},
         axis=1
     )
-    print(df.tail())
+    return df
 
 
 if __name__ == '__main__':
