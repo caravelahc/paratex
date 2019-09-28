24bf0aa0e56aecc047e04e98b9066b1481cf3653
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2019-09-28 20:20:43 -0300

    Refactor splitted methods into extractor.

--- paratex/extractor.py
+++ paratex/extractor.py
@@ -1,6 +1,6 @@
 '''Attendance extractor module.'''
 from dataclasses import dataclass
-from typing import Dict, Optional, Tuple
+from typing import Dict, List, Optional, Tuple
 
 from bs4 import BeautifulSoup
 from bs4.element import Tag
@@ -13,23 +13,21 @@ class Session:
     date: str
     attendance: Dict[str, Tuple[str, Optional[str]]]
 
-
-def url_from_id(session_id: int):
-    BASE_URL = (
-        'http://transparencia.alesc.sc.gov.br/'
-        'presenca_plenaria_detalhes.php'
-    )
-    return f'{BASE_URL}?id={session_id}'
-
-
-def load_html(url: str) -> str:
-    return requests.get(url).text
+    @staticmethod
+    def make_url(session_id: int):
+        BASE_URL = (
+            'http://transparencia.alesc.sc.gov.br/'
+            'presenca_plenaria_detalhes.php'
+        )
+        return f'{BASE_URL}?id={session_id}'
 
 
-def extract_attendance(session_id: int) -> Tuple[str, Dict[str, str]]:
-    '''Extracts every parliamentary's attendance in a given ALESC's meeting
-    attendance HTML page.'''
-    html = load_html(url_from_id(session_id))
+def extract_attendance(session_id: int) -> Session:
+    '''
+    Extracts every parliamentary's attendance in a given ALESC's meeting
+    attendance HTML page.
+    '''
+    html = load_html(Session.make_url(session_id))
     soup = BeautifulSoup(html, 'html.parser')
 
     title, date = find_session_header(soup)
@@ -52,6 +50,39 @@ def extract_attendance(session_id: int) -> Tuple[str, Dict[str, str]]:
     return Session(title, date, attendance)
 
 
+def last_sessions() -> List[Tuple[str, str]]:
+    '''
+    Retrieves a list of last month's session ids and their dates.
+    '''
+    url = 'http://transparencia.alesc.sc.gov.br/presenca_plenaria.php'
+    html = load_html(url)
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    table = soup.find('table', {'summary': "Presença dos Deputados"})
+    rows = table.findAll('tr', {'style': "text-align: center;"})
+
+    date_href_tuples = []
+    for tr in rows:
+        tds = tr.findAll('td')
+        date = tds[2].text
+        session_id = tds[3].find('a').attrs['href'].split('id=')[1]
+        date_href_tuples.append((date, session_id))
+
+    return date_href_tuples
+
+
+def load_html(url: str) -> str:
+    return requests.get(url).text
+
+
+def iter_rows(table: Tag):
+    '''Iter through a table's rows.'''
+    trs = table.find_all('tr')
+    for tr in trs:
+        yield tr.find_all('td')
+
+
 def find_session_header(soup: BeautifulSoup) -> Tuple[str, str]:
     '''Retrieves title and date from session page.'''
     title, date = soup.find(id='conteudo').h3.text.split('-')
@@ -61,10 +92,3 @@ def find_session_header(soup: BeautifulSoup) -> Tuple[str, str]:
 
 def find_attendance_table(soup: BeautifulSoup) -> Tag:
     return soup.find(id='conteudo').table
-
-
-def iter_rows(table: Tag):
-    '''Iter through a table's rows.'''
-    trs = table.find_all('tr')
-    for tr in trs:
-        yield tr.find_all('td')
