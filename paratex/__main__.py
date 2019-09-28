d1fd870b72abae0b1171c8f4370330e90c29bf32
Arthur Mesquita Pickcius <arthur.pickcius@gmail.com>
2019-09-28 17:32:26 -0300

    generate a list of URLs of each month of seasons

--- paratex/__main__.py
+++ paratex/__main__.py
@@ -2,12 +2,37 @@ import pandas as pd
 import requests
 import bs4
 from bs4 import BeautifulSoup
+from datetime import date
+import calendar
+
+
+def add_month(sourcedate):
+    month = sourcedate.month
+    year = sourcedate.year + month // 12
+    month = month % 12 + 1
+    day = min(sourcedate.day, calendar.monthrange(year, month)[1])
+    return date(year, month, day)
+
+
+def get_month_sessions() -> list:
+    dt = date(2011, 4, 1)
+    URL = []
+    while dt < date.today():
+        dt = add_month(dt)
+        URL.append(f"presenca_plenaria.php?periodo={dt.month:02}-{dt.year}")
+    return URL
+
 
 def main():
-    URL = 'http://transparencia.alesc.sc.gov.br/presenca_plenaria_detalhes.php?id=1783'
+    baseURL = "http://transparencia.alesc.sc.gov.br/"
+    URL = baseURL + "presenca_plenaria_detalhes.php?id=1783"
     html = load_html(URL)
     one_day_presences = parse(html)
     visualizing_results(one_day_presences)
+    
+    month_URL_list = get_month_sessions()
+    for monthURL in month_URL_list:
+        print(baseURL + monthURL)
 
 
 def load_html(url: str) -> str:
@@ -15,27 +40,28 @@ def load_html(url: str) -> str:
 
 
 def parse(html: str):
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, "html.parser")
 
-    aux = soup.find_all('tr')
-    entries = [i for i in aux if i.find('img') is None]
-    observations = [build_one_observation(entry, '20-10-2010') for entry in entries]
-    
+    aux = soup.find_all("tr")
+    entries = [i for i in aux if i.find("img") is None]
+    observations = [build_one_observation(entry, "20-10-2010") for entry in entries]
+
     return observations
 
-def build_one_observation(tr: bs4.element.Tag, date:str) -> (str, str, str, str):
+
+def build_one_observation(tr: bs4.element.Tag, date: str) -> (str, str, str, str):
     # (Name, Presence, Justification, Date)
-    td = tr.findAll('td') 
+    td = tr.findAll("td")
     name = td[0].text
-    
-    aux =  td[1]
-    if aux.find('a') == None:
+
+    aux = td[1]
+    if aux.find("a") is None:
         presence = aux.text.replace(" ", "").strip()
-        justification = 'x'
+        justification = "x"
     else:
-        presence = aux.find('a').text.strip()
-        justification = aux.find('div').text.strip()
-        
+        presence = aux.find("a").text.strip()
+        justification = aux.find("div").text.strip()
+
     return (name, presence, justification, date)
 
 
@@ -43,9 +69,9 @@ def build_one_observation(tr: bs4.element.Tag, date:str) -> (str, str, str, str)
 # will leave this here for utility purposes for now
 def visualizing_results(observations):
     df = pd.DataFrame(observations)
-    df = df.rename({0:'Nome', 1:'Presenca', 2:'Justificativa', 3:'Data'}, axis=1)
+    df = df.rename({0: "Nome", 1: "Presenca", 2: "Justificativa", 3: "Data"}, axis=1)
     print(df.tail())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
