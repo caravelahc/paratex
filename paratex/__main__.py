3cb69a7fc94500bcef2e2eca83fd248edc13dac3
Joao Vitor Maia <joaocampo2@hotmail.com>
2019-10-27 19:57:52 -0300

    Create data range for extraction, accepts start and end, if both are ommited fetch the last month sessions. Plot data from pandas to CSV file.

--- paratex/__main__.py
+++ paratex/__main__.py
@@ -1,17 +1,23 @@
 import pandas as pd
-
-from .extractor import extract_attendance, Session
-
+from datetime import date
+from extractor import extract_attendance, Session, fetch_sessions_from_interval
 
 def main():
-    session = extract_attendance(1783)
-    print(session.title)
-    print(as_dataframe(session).head())
+    attendances = []
+    sessions = fetch_sessions_from_interval(date(2011, 10, 15), date(2019, 10, 15))
+    
+    for month in sessions:
+        for session in month:
+            session_data = extract_attendance(session[0])
+            dataframe = session_as_dataframe(session_data)
+            attendances.append(dataframe)
 
+    for dataframe in attendances:
+        dataframe.to_csv(f"output/{dataframe.iloc[1,3]}.csv")
 
 # Intended to be used later on the code. Will leave this here for utility
 # purposes for now.
-def as_dataframe(session: Session):
+def session_as_dataframe(session: Session):
     observations = (
         (name, presence, justification, session.date)
         for name, (presence, justification) in session.attendance.items()
@@ -22,7 +28,6 @@ def as_dataframe(session: Session):
         axis=1
     )
     return df
-
 
 if __name__ == '__main__':
     main()
