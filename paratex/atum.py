1617afde4cf07f9d5c6ea2f0853a80996c3d33ed
Patrick José Pereira <patrickelectric@gmail.com>
2020-10-31 14:51:08 -0300

    Run black
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- paratex/atum.py
+++ paratex/atum.py
@@ -1,18 +1,18 @@
-'''rodem essa bodega com
+"""rodem essa bodega com
 "python -i atum.py" ou
 dai da pra ir rodando novos comandos no terminal
 e ir vendo como o selenium responde, eh bem top
 
 instalando geckodriver
 https://askubuntu.com/questions/851401/where-to-find-geckodriver-needed-by-selenium-python-package
-'''
+"""
 from selenium import webdriver
 
 
 class Atum:
     def __init__(self, driver):
         self.driver = driver
-        self.url = 'http://transparencia.alesc.sc.gov.br/presenca_plenaria.php'
+        self.url = "http://transparencia.alesc.sc.gov.br/presenca_plenaria.php"
 
         # formas de colocar as coisas que a gente quer de verdade
         # assim que a gente descobrir como elas se parecem
@@ -24,7 +24,7 @@ class Atum:
         self.driver.get(self.url)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ff = webdriver.Firefox()
     atum = Atum(ff)
     atum.navigate()
